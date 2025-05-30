ae9eea5347e6247e4b2947bbc2d6bac1ae53f2a1
Kemeow815 <kemiao@kmblog.icu>
2025-05-30 22:54:33 +0800

    初步同步

--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,32 @@
 import type { PostHideElements } from '~/content/config';
 
-export const Site = 'https://astro-blog-zozo.pages.dev';
+export const Site = 'https://blog-v8.kemeow.top';
 export const SiteLanguage = 'zh';
-export const SiteTitle = '无尽光芒';
-export const SiteDescription = '踏歌长行，梦想永在';
-export const FooterDescription = '肆意闪耀';
-export const AdminName = 'Ladit';
+export const SiteTitle = '喵落阁';
+export const SiteDescription = '愿你看清一切真相后，依旧热爱你的家人和朋友。';
+export const FooterDescription = '奇迹只能一时，命运总是漫长。';
+export const AdminName = '克喵爱吃卤面';
 export const PageSize = 15;
 
 // socialPlatform => userName
 // check components/Header.astro socialConfig for more info
 export const Socials: Record<string, Record<string, string>> = {
-	mail: { url: 'mailto:i@ladit.me' },
-	github: { url: 'https://github.com/ladit/astro-blog-zozo' },
-	x: { url: 'https://x.com/userName' },
-	mastodon: { url: 'https://mastodon.social/@userName' },
-	facebook: { url: 'https://facebook.com/userName' },
-	instagram: { url: 'https://instagram.com/userName' },
-	telegram: { url: 'https://t.me/@userName' },
-	youtube: { url: 'https://youtube.com/@userName' },
-	skype: { url: 'https://skype.com/userName' },
-	slack: { url: 'https://slack.com/userName' },
-	messenger: { url: 'https://messenger.com/userName' },
-	whatsapp: { url: 'https://whatsapp.com/userName' },
-	snapchat: { url: 'https://snapchat.com/userName' },
-	line: { url: 'https://line.com/userName' },
-	twitch: { url: 'https://twitch.com/userName' },
-	weibo: { url: 'https://weibo.com/userName' },
+	mail: { url: 'mailto:me@kemiaosw.top' },
+	github: { url: 'https://github.com/Kemeow815' },
+	x: { url: 'https://x.com/kemiaosw' },
+	// mastodon: { url: 'https://mastodon.social/@userName' },
+	// facebook: { url: 'https://facebook.com/userName' },
+	// instagram: { url: 'https://instagram.com/userName' },
+	telegram: { url: 'https://t.me/KemiaoJun' },
+	// youtube: { url: 'https://youtube.com/@userName' },
+	// skype: { url: 'https://skype.com/userName' },
+	// slack: { url: 'https://slack.com/userName' },
+	// messenger: { url: 'https://messenger.com/userName' },
+	// whatsapp: { url: 'https://whatsapp.com/userName' },
+	// snapchat: { url: 'https://snapchat.com/userName' },
+	// line: { url: 'https://line.com/userName' },
+	// twitch: { url: 'https://twitch.com/userName' },
+	// weibo: { url: 'https://weibo.com/userName' },
 	bilibili: { url: 'https://space.bilibili.com/userName' },
 	dingding: { url: 'https://dingtalk.com/userName' },
 	zhihu: { url: 'https://zhihu.com/userName' },
